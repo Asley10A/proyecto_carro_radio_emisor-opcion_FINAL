8f21e61b551100839bf8855233f4ea0bda5cc8b2
Asley10A <135353511+Asley10A@users.noreply.github.com>
2023-06-16 12:54:56 -0500

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,51 @@
+input.onGesture(Gesture.LogoUp, function () {
+    basic.showLeds(`
+        . . # . .
+        . # # # .
+        # . # . #
+        . . # . .
+        . . # . .
+        `)
+    radio.sendNumber(0)
+})
+input.onGesture(Gesture.ScreenUp, function () {
+    basic.showLeds(`
+        # . . . #
+        . # . # .
+        . . # . .
+        . # . # .
+        # . . . #
+        `)
+    radio.sendNumber(4)
+})
+input.onGesture(Gesture.TiltRight, function () {
+    basic.showLeds(`
+        . . # . .
+        . . . # .
+        # # # # #
+        . . . # .
+        . . # . .
+        `)
+    radio.sendNumber(2)
+})
+input.onGesture(Gesture.TiltLeft, function () {
+    basic.showLeds(`
+        . . # . .
+        . # . . .
+        # # # # #
+        . # . . .
+        . . # . .
+        `)
+    radio.sendNumber(3)
+})
+input.onGesture(Gesture.LogoDown, function () {
+    basic.showLeds(`
+        . . # . .
+        . . # . .
+        # . # . #
+        . # # # .
+        . . # . .
+        `)
+    radio.sendNumber(1)
+})
+radio.setGroup(27)
